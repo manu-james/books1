b3a4401ac6b909317783c01f2715cda617ff77bd
manu-james <mca524@rajagiri.edu>
2021-03-17 11:58:38 +0530

    message

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
         /* Modify the background color */ 
           
         .navbar-custom { 
-            background-color: lightgreen; 
+            background-color: rgb(44, 44, 44); 
         } 
         /* Modify brand and text color */ 
           
         .navbar-custom .navbar-brand, 
         .navbar-custom .navbar-text { 
-            color: green; 
+            color: rgb(255, 255, 255); 
         }
         
         #ABC {
@@ -30,45 +30,43 @@
             <nav class="navbar navbar-custom" >
                 <nav class="navbar navbar-expand-lg">
                     <div class="container-fluid">
-                      <a class="navbar-brand" href="#"><h3><p style="color: green;"></p>BOOKSY</h3>&nbsp;&nbsp;</a>
+                      <a class="navbar-brand" href="#"><h3><p style="color: rgb(0, 2, 128);"></p>BOOKS.COM</h3>&nbsp;&nbsp;</a>
                       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                       </button>
                       <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
                           <li class="nav-item">
-                            <a class="nav-link" href="#"></a><h4><p style="color: green;"></p></h4></a>
+                            <a class="nav-link" href="#"></a><h4><p style="color: rgb(0, 23, 128);"></p></h4></a>
                           </li>
                           <li class="nav-item">
-                            <a class="nav-link" href="#"></a><h4><p style="color: green;"></p></h4></a>
+                            <a class="nav-link" href="#"></a><h4><p style="color: rgb(19, 0, 128);"></p></h4></a>
                           </li>
                           <li class="nav-item">
                             &nbsp;&nbsp;<a class="nav-link active" aria-current="page" href="b.php"><h4><p style="color: green;">Home</p></h4></a>
                           </li>
                           <li class="nav-item">
-                            <a class="nav-link" href="#"></a><h4><p style="color: green;"></p></h4></a>
+                            <a class="nav-link" href="#"></a><h4><p style="color: rgb(0, 23, 128);"></p></h4></a>
                           </li>
                           <li class="nav-item">
-                            <a class="nav-link" href="#"></a><h4><p style="color: green;"></p></h4></a>
+                            <a class="nav-link" href="#"></a><h4><p style="color: rgb(0, 2, 128);"></p></h4></a>
                           </li>
                           <li class="nav-item">
-                            <a class="nav-link" href="b.php"></a><h4><p style="color: green;">Book</p></h4></a>
+                            <a class="nav-link" href="b.php"></a><h4><p style="color: rgb(28, 0, 128);">Book</p></h4></a>
                           </li>
                           <li class="nav-item">
-                            <a class="nav-link" href="#"><h4><p style="color: green;"></p></h4></a>
+                            <a class="nav-link" href="#"><h4><p style="color: rgb(0, 2, 128);"></p></h4></a>
                           </li>
                           <li class="nav-item">
-                            <a class="nav-link" href="#"></a><h4><p style="color: green;"></p></h4></a>
+                            <a class="nav-link" href="#"></a><h4><p style="color: rgb(9, 0, 128);"></p></h4></a>
                           </li>
                           <li class="nav-item">
-                            <a class="nav-link" href="b.php"><h4><p style="color: green;">About</p></h4></a>
+                            <a class="nav-link" href="b.php"><h4><p style="color: rgb(34, 0, 128);">About</p></h4></a>
                           </li>
                           <li class="nav-item">
-                            <a class="nav-link" href="#"></a><h4><p style="color: green;"></p></h4></a>
+                            <a class="nav-link" href="#"></a><h4><p style="color: rgb(13, 0, 128);"></p></h4></a>
                           </li>
-                          <li class="nav-item">
-                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"><img src="https://cdn1.vectorstock.com/i/1000x1000/51/35/creative-green-book-leaf-logo-vector-22495135.jpg" alt="" width="80"></a>
-                          </li>
+                          
                         </ul>
                       </div>
                     </div>
@@ -83,7 +81,7 @@
             <div class="col">
                 <figure class="text-center">
                     <blockquote class="blockquote">
-                      <h1><b><p style="color:rgb(33, 184, 19)">BOOKSY READS</p></b></h1>
+                      <h1><b><p style="color:rgb(55, 19, 184)">BOOKSREADS</p></b></h1>
                     </blockquote>
                     <figcaption class="blockquote-footer">
                        <cite title="Source Title">Live through books</cite>
@@ -98,10 +96,10 @@
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiBFMQBcLFjfrYggKR9eSLglAEu9eSYCYrgg&usqp=CAU" width="200">
+          <img src="" width="200">
         </div><br><br>
         <div class="col col-12 col-sm-6"><br><br>
-          <h5><b><i><p style="color: rgb(111, 189, 111);">“A great book should leave you with many experiences,<br>
+          <h5><b><i><p style="color: rgb(90, 90, 90);">“A great book should leave you with many experiences,<br>
             and slightly exhausted at the end.<br>
              You live several lives while reading.”</p>
             </i></b></h5>
@@ -121,21 +119,21 @@
                 </div>
                 <div class="carousel-inner">
                   <div class="carousel-item active">
-                    <img src="http://paimage.radio.gov.pk/20181112/1542019157.jpg" width=1200 alt="...">
+                    <img src="https://primexprinters.ph/wp-content/uploads/2017/02/Offset-Printing_Coffee-Table-Book_MG_4492_1000px.jpg" width=1200 alt="...">
                     <div class="carousel-caption d-none d-md-block">
                       <h5>First slide label</h5>
                       <p>Some representative placeholder content for the first slide.</p>
                     </div>
                   </div>
                   <div class="carousel-item">
-                    <img src="https://www.teahub.io/photos/full/17-171444_2880x1800-book-lovely-hd-wallpaper-book-wallpaper-for.jpg" width=1200 alt="...">
+                    <img src="https://www.onedarnleyroad.com/uploads/work/Abdulnasser/hi-res/Abdulnasser-Gharem-Art-Coffee-Table-Book-Design-One-Darnley-Road-5.jpg" width=1200 alt="...">
                     <div class="carousel-caption d-none d-md-block">
                       <h5>Second slide label</h5>
                       <p>Some representative placeholder content for the second slide.</p>
                     </div>
                   </div>
                   <div class="carousel-item">
-                    <img src="https://lh3.googleusercontent.com/proxy/_sA_OwRBUIsKSeQFyQtoY639Eb8ZVsYRUvvJkfV5KUy5Vu_tjS6ANXTZmVaTwiUf_nvVSca2EWvp_6LAjvFCd3WTZ3EWxh81QgGxEkgyhRFdpDkDutqotM04KQbg6zzcdfh3Ylz4Ka2SOY0gR1ZAYCyfw04-_-p-5-Rg" width=1200 alt="...">
+                    <img src="https://th.bing.com/th/id/OIP.tq8haVMHKQkCK_KXP6dJpQHaE8?pid=ImgDet&w=639&h=427&rs=1" width=1200 alt="...">
                     <div class="carousel-caption d-none d-md-block">
                       <h5>Third slide label</h5>
                       <p>Some representative placeholder content for the third slide.</p>
@@ -159,34 +157,11 @@
         
       </div><br><br><br>
       <div class="row">
-        <div class="col">
-          <h4>
-            <b>
-              <p style="color: green;font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;">
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              It’s an exciting time to be a book reviewer. Once confined to print newspapers and journals,<br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;reviews now dot many corridors of the Internet — forever helping others discover
-                their next great read.<br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                That said, every book reviewer will face a familiar
-                panic: how can you do justice to a great book in just a thousand  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;words?
-              </p>
-            </b>
-              <b>
-                <p style="font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;color: green;">
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                  As you know, the best way to learn how to do something is by immersing yourself in it. Luckily, the Internet<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                  &nbsp;&nbsp;&nbsp;&nbsp;(i.e. Goodreads and other review sites, in particular) has made book reviews more accessible than ever — 
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;which means that there are a lot of book reviews examples out there for you to view!
-                </p>
-              </b>
-              
-
+        <div class="col ">
+          <h5>
+          
             
-          </h4>
+          </h5>
         </div>
       </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>  
